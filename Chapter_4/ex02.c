848cbb3875862ca70c32d8b5ae8b44ce5880da69
2199216K <2199216K@student.gla.ac.uk>
2017-10-02 21:57:51 +0100

    Added Ch04 ex02.c

--- Chapter_4/ex02.c
+++ Chapter_4/ex02.c
@@ -2,6 +2,10 @@
 #include <ctype.h>
 #define MAXLINE 100
 
+/* Exercise 4-2. Extend atof to handle scientific notation of the form 123.45e-6
+  where a floating-point number may be followed by e or E and an optionally
+  signed exponent. */
+
 int mygetline(char line[], int max);
 double atof(char s[]);
 
